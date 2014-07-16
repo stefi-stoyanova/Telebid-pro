d0a98cbedef6d3a5ec32644855076deb12b5ee18
stefi-stoyanova <stefi.stoyanova95@gmail.com>
2014-07-16 22:14:29 +0300

    Update chisla.cc

--- chisla.cc
+++ chisla.cc
@@ -1,50 +1,48 @@
 #include <iostream>
-#include <string.h>
-#include <stdio.h>
-
+#include <math.h>
+#include <time.h>
 using namespace std;
 
-
-void lToA(long long n, char* buffer)
+int cntSymbols(long long n)
 {
-  char* tmp = new char [strlen(buffer)+1];
-  int i=0;
-  while (n)
-  {
-    tmp[i] = (n%10) + '0';
-    n/=10;
-    i++;
-  }
-  tmp[i]=0;
-
-  for(int k=strlen(tmp)-1, i=0; k>=0; k--, i++)
-    buffer[i]=tmp[k];
-  buffer[strlen(tmp)]=0;
-
-  delete []tmp;
-}
-int main() {
-
-  int n=-1;
-  cin >> n;
-  
-  char* buffer = new char[n+32];
-  char* tmp = new char[32];
-  buffer[0] = '1';
-  buffer[1] = 0;
-  
-  for(long long i=2; ; i++)
-  {
-      lToA(i*i, tmp);
-      strcat(buffer, tmp);
-    if(buffer[n-1])
+    int i=0;
+    while (n)
     {
-      cout << buffer[n-1] << endl;
-      break;
+        n/=10;
+        i++;
     }
-  }
-  delete []buffer;
-  delete []tmp;
+    return i;
+}
 
-  return 0;
+int getAtPos(long long n, int pos)
+{
+    return (n/ (int)pow(10, cntSymbols(n)-pos))%10;
+}
+
+
+int main()
+{
+    int n=-1;
+    do
+        cin >> n;
+    while(n<=0 || n>=3200000);
+
+    int cnt=0;
+
+    for (long long i=1; ;i++)
+    {
+        cnt+=cntSymbols(i*i);
+        if (cnt==n)
+        {
+           cout << (i*i)%10 << endl;
+           break;
+        }
+        else if(cnt>n)
+        {
+            cout << getAtPos(i*i, cnt-n) << endl;
+            break;
+        }
+    }
+
+    return 0;
 }
