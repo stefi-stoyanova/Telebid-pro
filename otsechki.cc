c764b33e9e379b6d1a756ed2060e7cd3562ef81b
stefiStoyanova <stefi_95_sf@abv.bg>
2014-07-24 14:58:38 +0300

    now it draws

--- otsechki.cc
+++ otsechki.cc
@@ -12,6 +12,7 @@ int main()
 	
 	do
 	{
+		cout << "Enter 4 numbers: \n";
 		cin >> n >> a >> b >>c;
 		if(!cin) 
 		{
@@ -22,28 +23,45 @@ int main()
 	}
 	while (!(n>0 && a>0 && b>0 && c>0 && n <100000&& a<100000 && b<100000 && c<100000));
 	
+	
+	ofstream out("otsechki.html");
+	out << "<html>\n<body>\n <table  width= \"80%\" borger=\"10\"><tr>";
+	
 	int metri=n;
 	
-	int* georgi =  new int[n/a + 1];
-	int * geri = new int [n/b+ 1];
+	int* arr =  new int[n+1];
+	for(int i=0; i<n+1; i++)
+		arr[i]=0;
 	
 	for(int i=0; a*i<=n; i++)
-		georgi[i]=a*i;
+		arr[a*i]=1;
 
 	for(int i=0; b*i<=n; i++ )
 	{
-		geri[i]=n - b*i;
-		for (int k=0; k<=n/a; k++)
+		if(arr[b*i]==1)
+			arr[b*i]=3;
+		else
+			arr[b*i]=2;		
+	}
+	
+	for(int i=0; i+c<n+1; i++)
+	{
+		if(arr[i] && arr[i+c] && (arr[i]!=arr[i+c] || arr[i]==3) )
 		{
-			if (geri[i]-georgi[k]==c || geri[i]-georgi[k]==-c)
-				metri -=c;
+			metri-=c;
+			out << "<td bgcolor = \"#FF0000\">\n";
+		}
+		else 
+		{
+			out << "<td bgcolor=\"#0000FF\"></td>\n";
 		}
 	}
+	
 	cout << metri << endl;
-	
-	delete []georgi;
-	delete []geri;
-	
+	out << "</tr></table></body>\n</html>";
+	out.close();
+	delete []arr;
+		
 	return 0;
 	
 }
